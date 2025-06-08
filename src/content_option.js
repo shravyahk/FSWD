9269ad23c329ad3da7c5b801fa7681f0644d0e9d
shravyahk <shkmce@gmail.com>
2025-06-08 15:55:54 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,103 @@
+const logotext = "Shravya";
+const meta = {
+    title: "Shravya H K",
+    description: "I’m Shravya H K Full stack devloper and AI prompt engineer",
+};
+
+const introdata = {
+    title: "I’m Shravya H K",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "I’m a full stack developer with a passion for creating dynamic and responsive web applications. I also specialize in AI prompt engineering, leveraging the power of AI to enhance user experiences.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "about my self",
+    aboutme: "I am a passionate Computer Science and Engineering student with a strong foundation in programming and software development. My key projects have sharpened my teamwork and problem-solving skills. I’m eager to apply my technical knowledge and enthusiasm for technology to create impactful software solutions and grow in the tech industry.",
+};
+const worktimeline = [{
+        jobtitle: "Full Stack Developer",
+        where: "Tech Company",
+        date: "2022 - Present",
+    },
+    {
+        jobtitle: "AI Prompt Engineer",
+        where: "AI Solutions Inc.",
+        date: "2021 - 2022",
+    },
+    {
+        jobtitle: "Software Intern",
+        where: "Startup Hub",
+        date: "2020 - 2021",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "c/c++",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Skilled in UI/UX design with expertise in wireframing, prototyping, and user research using Figma and Adobe XD. Strong understanding of usability principles, responsive design, and accessibility standards. Collaborative approach to creating intuitive interfaces that enhance user experience and meet business goals",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Skilled in mobile app design and development, with expertise in UI/UX, cross-platform frameworks (Flutter, React Native), and native development (Swift, Kotlin). Proficient in prototyping (Figma), API integration, and app deployment. Strong problem-solving and Agile methodology experience.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Skilled in WordPress design and development, specializing in custom themes, plugin integration, and responsive websites. Proficient in HTML, CSS, PHP, and JavaScript for customization. Experienced with Elementor, WooCommerce, and SEO optimization. Focused on performance, security, and user-friendly design.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://cdn.pixabay.com/photo/2018/10/01/09/21/pets-3715733_1280.jpg",
+        description: "pet adoption and care management",
+        link: "https://pawsconnect.infinityfreeapp.com/",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "shkmce@gmail.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Contact me for any inquiries or collaborations. I am always open to discussing new projects, creative ideas, or opportunities to be part of your vision.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    contactConfig, dataabout,
+    dataportfolio, introdata, logotext, meta, services, skills, socialprofils, worktimeline
+};
